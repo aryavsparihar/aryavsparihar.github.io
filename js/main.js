244584e8df9c65909a9f9607f4a03fe665f1cd3d
kamehameha <kamehameha@localhost.localdomain>
2025-06-20 15:05:24 +0530

    Restructure for clean URLs (folder-based routing)

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   const path = window.location.pathname;
+//   const routes = {
+//     '/': '/index.html',
+//     '/blog/': '/blog/index.html',
+//     '/walkthroughs/': '/walkthroughs/index.html',
+//     '/ctfs/': '/ctfs/index.html',
+//     '/contact/': '/contact/index.html'
+//   };
+//   const match = routes[path];
+//   if (match && path !== match) {
+//     window.location.replace(match);
+//   }
+// });
